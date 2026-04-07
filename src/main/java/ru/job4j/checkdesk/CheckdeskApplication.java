3c5419355065069cbf432a66fa438069affcfdb5
kir <kirpro95@yandex.ru>
2026-04-07 13:34:53 +0300

    initial commit

---
+++ src/main/java/ru/job4j/checkdesk/CheckdeskApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.checkdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckdeskApplication.class, args);
+	}
+
+}
